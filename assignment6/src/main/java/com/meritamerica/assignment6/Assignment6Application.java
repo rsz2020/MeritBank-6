696d2391e53c4cb272d359b694970ecc2d281054
Rufy Buddy <rufyzee@gmail.com>
2020-05-16 23:35:53 -0500

    Update
    Had to refork project. I had made ammendments in error to the final project. Now updating with minor corrections based on prompt

--- assignment6/src/main/java/com/meritamerica/assignment6/Assignment6Application.java
+++ assignment6/src/main/java/com/meritamerica/assignment6/Assignment6Application.java
@@ -8,6 +8,7 @@ public class Assignment6Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment6Application.class, args);
+		//System.out.println("Welcome");
 	}
 
 }
